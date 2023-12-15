da443a6942356fa2c013785020c0945c18d9ab00
Leofierus <leofierus@gmail.com>
2023-12-15 12:10:05 -0500

    Milestone-6: Docker base commit

--- foreign_whispers/settings.py
+++ foreign_whispers/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--_*oonk_4#bfme+l83_)5#g82-y2%u*#!*+ixpb=tft!b0t3+x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
